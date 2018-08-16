dbb7c80be4d4c6cd78447c1965ad9c6d1e8cfe8f
jamwal <42438105+jamwal-any@users.noreply.github.com>
2018-08-16 17:06:28 +0530

    Create url.php

---
+++ url.php
@@ -0,0 +1,4 @@
+https://www.codeigniter.com/user_guide/
+https://www.codeigniter.com/userguide3/overview/appflow.html
+https://www.codeigniter.com/userguide2/database/active_record.html
+https://www.codeigniter.com/user_guide/database/query_builder.html?highlight=update_batch
